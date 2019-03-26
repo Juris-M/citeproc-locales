83e2bbb2c9624ab9a5f4e48a2cd9dc9262162adb
Frank Bennett <biercenator@gmail.com>
2019-03-26 10:08:15 +0900

    Simplify

--- getLocalePath.js
+++ getLocalePath.js
@@ -1,5 +1,3 @@
-require("path");
-function getLocalePath () {
-    return path.join(__dirname, "locales");
-}
-module.exports = getLocalePath();
+var path = require("path");
+var localePath = path.join(__dirname, "locales");
+module.exports = localePath;
